9ee74a443dc3848a63985603cf5ba9c07316e5c3
Ricardo Arnez <ricardoarnez@hotmail.com>
2022-06-04 11:12:28 -0400

    test

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
+
 
 public class HomePage extends BasePage {
     protected WebDriver driver;
